39e0b5b6be647b2888f1d3654502974bbaae1c10
CatMeowByte <CatMeowByte@gmail.com>
2025-12-08 19:35:32 +0900

    the specific cent of the related control is old

--- inject.js
+++ inject.js
@@ -1,6 +1,25 @@
 // inject
-let _fs=false;
-Element.prototype.requestFullscreen=function(){window.parent.postMessage({action:'requestFullscreen'},'*');};
-document.exitFullscreen=function(){window.parent.postMessage({action:'exitFullscreen'},'*');};
-Object.defineProperty(document,'fullscreenElement',{get:()=>_fs?document.body:null});
-window.addEventListener('message',e=>{if(e.data.action==='fullscreenState')_fs=e.data.value;});
+let _fs = false;
+
+Element.prototype.requestFullscreen = function () {
+ window.parent.postMessage({ action: 'requestFullscreen' }, '*');
+};
+
+document.exitFullscreen = function () {
+ window.parent.postMessage({ action: 'exitFullscreen' }, '*');
+};
+
+Object.defineProperty(document, 'fullscreenElement', {
+ get: () => _fs ? document.body : null
+});
+
+window.addEventListener('message', e => {
+ if (e.data.action === 'fullscreenState') _fs = e.data.value;
+});
+
+const get_current_domain = () =>
+ window.location.pathname.match(/\/website\/([^\/]+)/)?.[1] || '';
+
+window.navigate = (url) => {
+  window.parent.postMessage({action: 'navigate', url: url}, '*');
+};
